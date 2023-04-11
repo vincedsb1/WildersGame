5bdd5b7376ccef64d0d7d7a8207fc2bad22c645b
Boivinos <boivinantonin@gmail.com>
2023-04-11 16:59:46 +0200

    WG07 homepage design

---
+++ frontend/src/components/Home/StartButton/StartButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./StartButton.scss";
+
+function StartButton() {
+  return (
+    <div className="StartButton">
+      <button type="button">Start</button>
+    </div>
+  );
+}
+
+export default StartButton;
