16c43250816d30e07d05e0d6dfa0abf5c3cbf4f1
Boivinos <boivinantonin@gmail.com>
2023-04-13 16:34:32 +0200

    WG073

--- frontend/src/components/GamePage/GamePage.jsx
+++ frontend/src/components/GamePage/GamePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Score from "./Header/Score";
 import Timer from "./Header/Timer";
+import BlocDeReponse from "./BlocDeReponse/BlocDeReponse";
 
 function GamePage() {
   return (
@@ -20,6 +21,7 @@ function GamePage() {
         />
       </div>
       <h2>It's a me, mario !</h2>
+      <BlocDeReponse />
     </div>
   );
 }
