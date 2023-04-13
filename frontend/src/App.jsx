138d525c8a198699f5547876f31a91341060965d
Vincent DESBROSSES <vincedsb@gmail.com>
2023-04-13 09:52:00 +0200

    WG070 - Page 2 - Countdown + Style + react-router-dom pour la redirection des pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import Home from "@components/Home/Home";
+import { Route, Routes } from "react-router-dom";
+import Countdown from "./components/Countdown/Countdown";
+import Game from "./components/Game/Game";
 import "./fonts.scss";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} className="App" />
+      <Route path="/countdown" element={<Countdown />} />
+      <Route path="/game" element={<Game />} />
+    </Routes>
   );
 }
 export default App;
