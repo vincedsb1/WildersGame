37342aa9cd8ba98a00a31809b498f0ebd688f63a
Boivinos <boivinantonin@gmail.com>
2023-04-13 11:42:40 +0200

    fixed conflicts w/ new scss page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import Home from "@components/Home/Home";
 import { Route, Routes } from "react-router-dom";
 import Countdown from "./components/Countdown/Countdown";
 import Game from "./components/Game/Game";
+import "./App.scss";
 
 function App() {
   return (
