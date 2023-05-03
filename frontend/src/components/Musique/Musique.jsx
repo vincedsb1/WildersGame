90ee4e773358b2217ec3f4eb7ef35c140ee7d791
ms.vergnaud@gmail.com <ms.vergnaud@gmail.com>
2023-05-03 15:25:24 +0200

    son uniquement sur game page

--- frontend/src/components/Musique/Musique.jsx
+++ frontend/src/components/Musique/Musique.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
 import { useRef } from "react";
+import { useLocation } from "react-router-dom";
 import musicFileMp3 from "./music1.mp3";
 import musicFileOgg from "./music1.ogg";
 import musicFileWav from "./music1.wav";
 
-// chnager la source des balaise audio
 function Musique({ isMuted, setIsMuted, volume, setVolume }) {
   const audioRef = useRef(null);
 
@@ -22,48 +22,101 @@ function Musique({ isMuted, setIsMuted, volume, setVolume }) {
     }
   };
 
+  const loc = useLocation();
+  const chemin = loc.pathname;
+  if (chemin === "/gameMode") {
+    return (
+      <div className="contenerMusique">
+        <div className="Musique">
+          <br />
+
+          <div className="audioBlock">
+            <input
+              className={isMuted && "hidden"}
+              id="volumeslider"
+              type="range"
+              min="0"
+              max="1"
+              value={volume}
+              onChange={handleChangeVolume}
+              step="0.01"
+            />
+
+            <img
+              src={
+                isMuted
+                  ? "../src/assets/Musique/SonOffImg.svg"
+                  : "../src/assets/Musique/SonOnImg.svg"
+              }
+              className={isMuted ? "MusicOff" : "MusicOn"}
+              alt={isMuted ? "musicOff" : "musicOn"}
+              onClick={handleMusicOn}
+              // onKeyDown={handleMusicOn}
+              role="presentation"
+            />
+            <audio
+              ref={audioRef}
+              id="where"
+              // src={musicFile}
+              loop
+              autoPlay
+              onTimeUpdate={handleUpdateVolume}
+              muted={isMuted}
+            >
+              <source src={musicFileMp3} type="audio/mp3" />
+              <source src={musicFileOgg} type="audio/ogg" />
+              <source src={musicFileWav} type="audio/wav" />
+              <track kind="captions" />
+            </audio>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="Musique">
-      <br />
+      <div className={Musique && "hiddenMusique"}>
+        <br />
+        <div className="audioBlock">
+          <input
+            className={isMuted && "hidden"}
+            id="volumeslider"
+            type="range"
+            min="0"
+            max="1"
+            value={volume}
+            onChange={handleChangeVolume}
+            step="0.01"
+          />
 
-      <div className="audioBlock">
-        <input
-          className={isMuted && "hidden"}
-          id="volumeslider"
-          type="range"
-          min="0"
-          max="1"
-          value={volume}
-          onChange={handleChangeVolume}
-          step="0.01"
-        />
-
-        <img
-          src={
-            isMuted
-              ? "../src/assets/Musique/SonOffImg.svg"
-              : "../src/assets/Musique/SonOnImg.svg"
-          }
-          className={isMuted ? "MusicOff" : "MusicOn"}
-          alt={isMuted ? "musicOff" : "musicOn"}
-          onClick={handleMusicOn}
-          // onKeyDown={handleMusicOn}
-          role="presentation"
-        />
-        <audio
-          ref={audioRef}
-          id="where"
-          // src={musicFile}
-          loop
-          autoPlay
-          onTimeUpdate={handleUpdateVolume}
-          muted={isMuted}
-        >
-          <source src={musicFileMp3} type="audio/mp3" />
-          <source src={musicFileOgg} type="audio/ogg" />
-          <source src={musicFileWav} type="audio/wav" />
-          <track kind="captions" />
-        </audio>
+          <img
+            src={
+              isMuted
+                ? "../src/assets/Musique/SonOffImg.svg"
+                : "../src/assets/Musique/SonOnImg.svg"
+            }
+            className={isMuted ? "MusicOff" : "MusicOn"}
+            alt={isMuted ? "musicOff" : "musicOn"}
+            onClick={handleMusicOn}
+            // onKeyDown={handleMusicOn}
+            role="presentation"
+          />
+          <audio
+            ref={audioRef}
+            id="where"
+            // src={musicFile}
+            loop
+            autoPlay
+            onTimeUpdate={handleUpdateVolume}
+            muted={isMuted}
+          >
+            <source src={musicFileMp3} type="audio/mp3" />
+            <source src={musicFileOgg} type="audio/ogg" />
+            <source src={musicFileWav} type="audio/wav" />
+            <track kind="captions" />
+          </audio>
+        </div>
       </div>
     </div>
   );
